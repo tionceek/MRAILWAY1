0bc2416b758f65e839587683188db58dedb5df8a
tionceek <rustamovurinboy@gmail.com>
2025-06-25 18:31:52 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,9 @@
 import os
 import requests
-from dotenv import load_dotenv  # .env fayldan o‘qish uchun
 
-# .env fayldagi o‘zgaruvchilarni yuklash
-load_dotenv()
-
-# Token va chat ID ni olish
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-CHAT_ID = os.getenv("CHAT_ID")
+# Railway muhitida .env kerak emas
+TOKEN = os.environ["TELEGRAM_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
 
 def send_telegram_message(name, phone, address, time):
     message = (
@@ -19,7 +15,5 @@ def send_telegram_message(name, phone, address, time):
     )
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     response = requests.post(url, data={"chat_id": CHAT_ID, "text": message})
-
-    # Xatolikni tekshirish (agar kerak bo‘lsa log chiqarish)
     if response.status_code != 200:
         print("Telegram xabari yuborishda xatolik:", response.text)
