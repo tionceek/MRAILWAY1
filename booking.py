0fe7e6fbb9353d09992de35015b2fd89df32a9bd
tionceek <rustamovurinboy@gmail.com>
2025-06-25 18:38:43 +0500

    Update booking.py

--- booking.py
+++ booking.py
@@ -1,15 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-available_slots = [
-    f"{hour:02d}:00" for hour in range(10, 18)
-]
-
+available_slots = [f"{hour:02}:00" for hour in range(10, 19)]
 booked = {}
 
 def book_slot(time, name):
     if time not in available_slots:
-        return False, "Noto‘g‘ri vaqt"
+        return False, "Bunday vaqt mavjud emas."
     if time in booked:
-        return False, "Bu vaqt allaqachon band qilingan"
+        return False, "Bu vaqt band qilingan."
     booked[time] = name
-    return True, "Band qilindi"
+    return True, "Band qilindi ✅"
